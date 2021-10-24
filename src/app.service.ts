2f04a35e5bdd23052d071f48c31502046e9067e7
Bruno Diaz <brunjo12@hotmail.com>
2021-10-24 07:02:30 -0500

    save 2

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,20 +3,13 @@ import { Client } from 'pg';
 
 @Injectable()
 export class AppService {
-  constructor(@Inject('PG') private clientPg: Client) {}
+  constructor(@Inject('PG') private clientPg: Client) { }
 
   getHello(): string {
     return 'Hello World!';
   }
 
   getConnection() {
-    return new Promise((resolve, reject) => {
-      this.clientPg.query('select * from product', (err, res) => {
-        if (err) {
-          reject(err);
-        }
-        resolve(res.rows);
-      });
-    });
+    return null;
   }
 }
